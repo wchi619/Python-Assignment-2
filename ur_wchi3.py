26d6a1f17e86729ead0ff2c148820c45b6b75ed1
William Chi <54991733+wchi619@users.noreply.github.com>
2020-08-16 20:11:19 -0400

    Update ur_wchi3.py

--- ur_wchi3.py
+++ ur_wchi3.py
@@ -4,7 +4,6 @@ OPS435 Assignment 2 - Fall 2019
 Program: ur_wchi3.py
 Author: William Chi
 
-The python code in this file ur_wchi3.py is original work written by William Chi. No code in this file is copied from any other source including any person, textbook, or on-line resources except those provided by the course instructor. I have not shared this python file with anyone or anything except for submission for grading. I understand that the Academic Honesty Policy will be enforced and violators will be reported and appropriate action will be taken.
 """
 
 import os
